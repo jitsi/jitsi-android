42c418ebf64ca20182d18dbc5cadb12c9ddbff94
paweldomas <pawel.domas@jitsi.org>
2013-08-12 08:43:18 +0200

    Makes possible to add new account from account settings screen.

--- src/org/jitsi/android/gui/account/AccountEnableActivity.java
+++ src/org/jitsi/android/gui/account/AccountEnableActivity.java
@@ -89,6 +89,33 @@ public class AccountEnableActivity
     }
 
     /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.account_settings_menu, menu);
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.add_account)
+        {
+            startActivity(AccountLoginActivity.class);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
      * Initializes the accounts table.
      */
     private void accountsInit()
