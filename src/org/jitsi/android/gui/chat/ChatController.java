0f0d88c7086d987f4e649d8d1bd9dfc369928e40
paweldomas <pawel.domas@jitsi.org>
2013-12-04 12:27:48 +0100

    Fixes invisible cursor in chat edit text. Does not clear edit text focus when message correction is cancelled to avoid confusion.

--- src/org/jitsi/android/gui/chat/ChatController.java
+++ src/org/jitsi/android/gui/chat/ChatController.java
@@ -262,7 +262,6 @@ public class ChatController
         {
             session.setCorrectionUID(null);
             msgEdit.setText("");
-            msgEdit.clearFocus();
             updateCorrectionState();
         }
     }
