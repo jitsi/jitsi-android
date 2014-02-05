6622a1eed05ed53b226e80a309d8cdea98c92080
paweldomas <pawel.domas@jitsi.org>
2014-02-05 17:51:50 +0100

    Updates jitsi bundles. Creates unit-tests project(IntelliJ for now).

---
+++ tests/src/org/jitsi/android/test/SampleTest.java
@@ -0,0 +1,18 @@
+package org.jitsi.android.test;
+
+
+import android.test.suitebuilder.annotation.*;
+import junit.framework.*;
+
+/**
+ *
+ */
+public class SampleTest
+    extends TestCase
+{
+    @SmallTest
+    public void testAdvancedTest1()
+    {
+        assertEquals("test1", true, true);
+    }
+}
